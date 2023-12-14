af984380a8e52c35f804359a039148cfc70175b6
MGrushenkov <MGrushenkov@egartech.ru>
2023-12-14 23:54:26 +0400

    web lab8 end

--- Lab8/Ex1/src/script.js
+++ Lab8/Ex1/src/script.js
@@ -1,19 +1,19 @@
-import { fromEvent, merge } from 'rxjs';
+import { fromEvent, merge } from 'rxjs'
 
-const firstBtn = document.getElementById("first");
-const secondBtn = document.getElementById("second");
-const thirdBtn = document.getElementById("third");
+const firstBtn = document.getElementById('first')
+const secondBtn = document.getElementById('second')
+const thirdBtn = document.getElementById('third')
 
 merge(
-fromEvent(firstBtn, "click"),
-fromEvent(secondBtn, "click"),
-fromEvent(thirdBtn, "click")
+    fromEvent(firstBtn, 'click'),
+    fromEvent(secondBtn, 'click'),
+    fromEvent(thirdBtn, 'click')
 ).subscribe(() => {
-function rand() {
-return Math.floor(Math.random() * 256);
-}
-function set_background_color() {
-document.body.style.backgroundColor = "rgb(" + rand() + "," + rand() + "," + rand() + ")";
-}
-set_background_color();
-});
+    function rand() {
+        return Math.floor(Math.random() * 256)
+    }
+    function set_background_color() {
+        document.body.style.backgroundColor = 'rgb(' + rand() + ',' + rand() + ',' + rand() + ')'
+    }
+    set_background_color()
+})
